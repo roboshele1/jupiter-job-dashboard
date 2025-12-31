5626ff124828d702af99ad12a520a0970319b873
Edwin Theadoos Akosua Adoo <theadoos@Edwin--Akosua-Adoo.local>
2025-12-31 10:48:13 -0400

    GROWTH_ENGINE_V1_IPC_REGISTERED

--- electron/ipc/registerIpc.js
+++ electron/ipc/registerIpc.js
@@ -1,9 +1,11 @@
 kconst { registerPortfolioIpc } = require("./portfolioIpc");
 const { registerIntelligenceIpc } = require("./intelligenceIpc");
+const { registerGrowthEngineIpc } = require("./growthEngineIpc");
 
 function registerAllIpc(ipcMain) {
   registerPortfolioIpc(ipcMain);
   registerIntelligenceIpc(ipcMain);
+  registerGrowthEngineIpc(ipcMain);
 }
 
 module.exports = { registerAllIpc };
